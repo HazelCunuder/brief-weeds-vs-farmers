bbac3a39a8c0b0d0b53252c30e3e1f3b24b7e29b
HazelCunuder <basile.cunuder@gmail.com>
2025-07-03 11:24:04 +0200

    feat: add DisplayMenu method

--- Farmer-vs-weeds/Menu/Menu.cs
+++ Farmer-vs-weeds/Menu/Menu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,43 @@ namespace Farmer_vs_weeds.Menu
 {
     internal class Menu
     {
+        // -- Properties --
+
+        // -- Methods --
+        public static void DisplayMenu()
+        {
+            bool isMenuOn = true;
+
+            while (isMenuOn)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("--- Brawling Farmers ---\n");
+                Console.WriteLine("1 - Add Farmer");
+                Console.WriteLine("2 - Remove a Farmer");
+                Console.WriteLine("3 - Show Farmer's list");
+                Console.WriteLine("4 - Start a tournament");
+                Console.WriteLine("5 - Show previous winners\n");
+                Console.WriteLine("0 - Quit");
+                Console.ResetColor();
+
+                MenuControls();
+            }
+            
+            
+            void MenuControls()
+            {
+                char input;
+                input = Console.ReadKey().KeyChar;
+
+                switch (input)
+                {
+                    case '0':
+                    case 'à':
+                        Console.Clear();
+                        isMenuOn = false;
+                        break;
+                }
+            }
+        }
     }
 }
