05ae6c13e202c232a87b464adf6eded769b711b5
HazelCunuder <basile.cunuder@gmail.com>
2025-07-03 10:52:28 +0200

    refactor: fix conflicts

---
+++ Farmer-vs-weeds/Farmers/TractorFarmer.cs
@@ -0,0 +1,75 @@
+﻿namespace Farmer_vs_weeds.Farmers
+{
+    internal class TractorFarmer : Farmer
+    {
+        // -- Attributes --
+
+        private bool inTractor;
+
+        // -- Constructor Statement --
+        public TractorFarmer(string username, int healthPoints, int attackDices, bool isInTractor) : base(username, healthPoints, attackDices)
+        {
+            inTractor = isInTractor;
+
+            bool correctTractorFarmerHP = false;
+
+            while (!correctTractorFarmerHP)
+            {
+
+                if (healthPoints < 0)
+                {
+                    Console.WriteLine("You cannot create a Automatic farmer with negative health\n");
+                    Console.Write("Please write a correct HP value: ");
+                    healthPoints = Convert.ToInt32(Console.ReadLine());
+                    continue;
+                }
+                else if (healthPoints < 100)
+                {
+                    Console.WriteLine("You cannot create a Automic Farmer with less than 100HP\n");
+                    Console.Write("Please write a correct HP value: ");
+                    healthPoints = Convert.ToInt32(Console.ReadLine());
+                    continue;
+                }
+                else if (healthPoints > 150)
+                {
+                    Console.WriteLine("You cannot create a Automatic Farmer with more than 150HP\n");
+                    Console.Write("Please write a correct HP value: ");
+                    healthPoints = Convert.ToInt32(Console.ReadLine());
+                    continue;
+                }
+                else
+                {
+                    this.SetHealthPoints(healthPoints);
+                    correctTractorFarmerHP = true;
+                }
+            }
+        }
+
+        // -- Methods --
+        public override void TakeDamage(int damage)
+        {
+            if (inTractor == true)
+            {
+                Console.WriteLine($"{GetUsername()} is in the tractor, they will now take half damage");
+
+                int currentHP = GetHPs();
+                SetHealthPoints(currentHP - damage / 2);
+            }
+
+        }
+
+        public override int Attack()
+        {
+            Random dice = new Random();
+            int rollTotal = 0;
+            int attackDice = dice.Next(1, 6);
+
+            for (int i = 0; i < GetAttackDices(); i++)
+            {
+                rollTotal += attackDice;
+            }
+
+            return rollTotal;
+        }
+    }
+}
--- Farmer-vs-weeds/Program.cs
+++ Farmer-vs-weeds/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool isFightOver = false;
+
+            TractorFarmer john = new TractorFarmer("John", 150, 1,true);
+            ChemFarmer terry = new ChemFarmer("Terry", 90, 9);
+
+            while (!isFightOver)
+            {
+                if (john.GetHPs() > 0 && terry.GetHPs() > 0)
+                {
+                    terry.TakeDamage(john.Attack());
+                    terry.ShowInfos();
+
+                    if (john.GetHPs() <= 0 || terry.GetHPs() <= 0)
+                    {
+                        string winner;
+                        if (john.GetHPs() > terry.GetHPs())
+                        {
+                            winner = "John";
+                        }
+                        else
+                        {
+                            winner = "Terry";
+                        }
+                        Console.WriteLine(winner + " has won the fight!");
+                        isFightOver = true;
+                    }
+                    else
+                    {
+                        john.TakeDamage(terry.Attack());
+                        john.ShowInfos();
+                    }
+                }
+                else if (john.GetHPs() <= 0)
+                {
+                    string winner;
+                    if (john.GetHPs() > terry.GetHPs())
+                    {
+                        winner = "John";
+                    }
+                    else
+                    {
+                        winner = "Terry";
+                    }
+                    Console.WriteLine(winner + " has won the fight!");
+                    isFightOver = true;
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
