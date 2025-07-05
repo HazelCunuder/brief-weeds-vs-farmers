0ad01f2ff645111a8589d5b2b99451167e686cd4
HazelCunuder <basile.cunuder@gmail.com>
2025-07-05 12:10:57 +0200

    feat(menu): added feature making the menu interactive with arrow keys

--- Farmer-vs-weeds/Menu/Menu.cs
+++ Farmer-vs-weeds/Menu/Menu.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using Farmer_vs_weeds.Farmers;
-
-namespace Farmer_vs_weeds.Menu
+﻿namespace Farmer_vs_weeds.Menu
 {
     internal class Menu
     {
@@ -22,71 +13,164 @@ namespace Farmer_vs_weeds.Menu
         public static void DisplayMenu()
         {
             bool isMenuOn = true;
+            int menuSelect = 0;
+            string[] menuContent = new string[] {
+                "╔═══════════════════════════════╗",
+                "║        Brawling Farmers       ║",
+                "║═══════════════════════════════║",
+                "║                               ║",
+                "║ 1 - Add Farmer                ║",
+                "║ 2 - Remove Farmer             ║",
+                "║ 3 - Show Farmer's List        ║",
+                "║ 4 - Start a tournament        ║",
+                "║ 5 - Start Singles Fight       ║",
+                "║ 6 - Show previous Winners     ║",
+                "║                               ║",
+                "║ 0 - Quit                      ║",
+                "╚═══════════════════════════════╝"
+            };
+
+            Console.CursorVisible = false;
 
             while (isMenuOn)
             {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("╔═══════════════════════════════╗");
-                Console.WriteLine("║        Brawling Farmers       ║");
-                Console.WriteLine("║═══════════════════════════════║");
-                Console.WriteLine("║                               ║");
-                Console.WriteLine("║ 1 - Add Farmer                ║");
-                Console.WriteLine("║ 2 - Remove Farmer             ║");
-                Console.WriteLine("║ 3 - Show Farmer's List        ║");
-                Console.WriteLine("║ 4 - Start a tournament        ║");
-                Console.WriteLine("║ 5 - Start Singles Fight       ║");
-                Console.WriteLine("║ 6 - Show previous Winners     ║");
-                Console.WriteLine("║                               ║");
-                Console.WriteLine("║ 0 - Quit                      ║");
-                Console.WriteLine("╚═══════════════════════════════╝");
+                Console.Clear();
 
-                Console.ResetColor();
+                int consoleWidth = Console.WindowWidth;
+                int consoleHeight = Console.WindowHeight;
+
+                int menuHeight = menuContent.Length;
+                int verticalPadding = (consoleHeight - menuHeight) / 2;
+
+                // -- Add blank lines for vertical centering --
+                for (int i = 0; i < verticalPadding; i++)
+                {
+                    Console.WriteLine();
+                }
+
+                for (int i = 0; i < menuContent.Length; i++)
+                {
+                    int leftPadding = (consoleWidth - menuContent[i].Length) / 2;
+
+                    Console.SetCursorPosition(leftPadding < 0 ? 0 : leftPadding, Console.CursorTop);
+
+                    if (i == menuSelect)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(menuContent[i]);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine(menuContent[i]);
+                        Console.ResetColor();
+                    }
+                }
 
                 MenuControls();
             }
-            
-            
+
             void MenuControls()
             {
-                char input;
-                input = Console.ReadKey().KeyChar;
+                ConsoleKeyInfo keyPressed = Console.ReadKey();
+                
 
-                switch (input)
+                if (keyPressed.Key == ConsoleKey.DownArrow && menuSelect != menuContent.Length - 1)
                 {
-                    case '1':
-                    case '&':
-                        Console.Clear();
-                        isMenuOn = false;
-                        AddFarmerMenu.AddFarmer();
-                        break;
-                    case '2':
-                    case 'é':
-                        Console.Clear();
-                        isMenuOn = false;
-                        RemoveFarmer.RemoveFarmerMenu();
-                        break;
-                    case '3':
-                    case '\"':
-                        Console.Clear();
-                        isMenuOn = false;
-                        DisplayList.DisplayListMenu();
-                        break;
-                    case '4':
-                    case '\'':
-                        Console.Clear();
-                        isMenuOn = false;
-                        Tournament.TournamentMenu();
-                        break;
-                    case '0':
-                    case 'à':
-                        Console.Clear();
-                        isMenuOn = false;
-                        break;
-                    default:
-                        Console.Clear();
-                        break;
+                    menuSelect++;
+                    Console.Clear();
                 }
+                else if (keyPressed.Key == ConsoleKey.UpArrow && menuSelect >= 1)
+                {
+                    menuSelect--;
+                    Console.Clear();
+                }
+                else if (keyPressed.Key == ConsoleKey.Enter)
+                {
+                    switch (menuSelect)
+                    {
+                        case 4:
+                            Console.Clear();
+                            isMenuOn = false;
+                            AddFarmerMenu.AddFarmer();
+                            break;
+                        case 5:
+                            Console.Clear();
+                            isMenuOn = false;
+                            RemoveFarmer.RemoveFarmerMenu();
+                            break;
+                        case 6:
+                            Console.Clear();
+                            isMenuOn = false;
+                            DisplayList.DisplayListMenu();
+                            break;
+                        case 7:
+                            Console.Clear();
+                            isMenuOn = false;
+                            Tournament.TournamentMenu();
+                            break;
+                        case 10:
+                            Console.Clear();
+                            isMenuOn = false;
+                            break;
+                        default:
+                            Console.Clear();
+                            break;
+                    }
+                }
+                else 
+                {
+                    switch (keyPressed.Key)
+                    {
+                        case ConsoleKey.D1:
+                            Console.Clear();
+                            isMenuOn = false;
+                            AddFarmerMenu.AddFarmer();
+                            break;
+                        case ConsoleKey.D2:
+                            Console.Clear();
+                            isMenuOn = false;
+                            RemoveFarmer.RemoveFarmerMenu();
+                            break;
+                        case ConsoleKey.D3:
+                            Console.Clear();
+                            isMenuOn = false;
+                            DisplayList.DisplayListMenu();
+                            break;
+                        case ConsoleKey.D4:
+                            Console.Clear();
+                            isMenuOn = false;
+                            Tournament.TournamentMenu();
+                            break;
+                        case ConsoleKey.D0:
+                            Console.Clear();
+                            isMenuOn = false;
+                            break;
+                        default:
+                            Console.Clear();
+                            break;
+                    }
+                } 
             }
         }
+
+        private static void PrintLinesInCenter(params string[] lines)
+        {
+            int verticalStart = (Console.WindowHeight - lines.Length) / 2;
+            int verticalPosition = verticalStart;
+
+            foreach (var line in lines)
+            {
+                int horizontalStart = (Console.WindowWidth - line.Length) / 2;
+
+                Console.SetCursorPosition(horizontalStart, verticalPosition);
+
+                Console.Write(line);
+
+                ++verticalPosition;
+            }
+        }
+
     }
 }
