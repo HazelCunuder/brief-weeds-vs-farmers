1e8edc131c3edf7cd67c07861f4465348ec35e0b
Loicwr <nephtyse19@hotmail.fr>
2025-07-07 09:44:28 +0200

    refactor (Menu) : add class redirection

--- Farmer-vs-weeds/Menu/AddFarmerMenu.cs
+++ Farmer-vs-weeds/Menu/AddFarmerMenu.cs
@@ -68,7 +68,7 @@
 
                         case 6:
 
-                            AddChemFarmer.ChemFarmer();
+                            AddTractorFarmer.TractorFarmer();
                             break;
 
                         case 8:
@@ -90,139 +90,17 @@
                     {
                         case ConsoleKey.D1:
                         case ConsoleKey.NumPad1:
-                            Console.Clear();
-                            types = "Classic Farmer";
-                            Console.Clear();
-                            Console.Write("\nName: ");
-                            username = Console.ReadLine();
-
-                            while (!validHp)
-                            {
-                                Console.WriteLine("\nChoose between 70 and 120 life points ");
-                                int inputUser = Convert.ToInt32(Console.ReadLine());
-
-                                if (inputUser < 70 || inputUser > 120)
-                                {
-                                    Console.WriteLine("Error,follow the instructions ");
-                                }
-                                else
-                                {
-                                    hp = inputUser;
-                                    break;
-                                }
-
-                            }
-                            while (!validAttackDices)
-                            {
-                                Console.WriteLine("\nChoose the number of attack dice between 4-6 d6");
-                                int inputUser = Convert.ToInt32(Console.ReadLine());
-
-                                if (inputUser < 3 || inputUser > 6)
-                                {
-                                    Console.WriteLine("Error,follow the instructions ");
-                                }
-                                else
-                                {
-                                    attackDices = inputUser;
-                                    break;
-                                }
-                            }
-
-                            Menu.FarmersList().Add(new Farmer(username, hp, attackDices, types));
-                            Console.Clear();
-                            Console.WriteLine($"\n{types} {username} create with {hp} HP and {attackDices} attack\n");
+                            AddSimpleFarmer.SimpleFarmer();
                             break;
 
                         case ConsoleKey.D2:
                         case ConsoleKey.NumPad2:
-
-                            types = "Chem Farmer";
-                            Console.Clear();
-                            Console.Write("\nName: ");
-                            username = Console.ReadLine();
-
-                            while (!validHp)
-                            {
-                                Console.WriteLine("\nChoose between 40 and 90 life points ");
-                                int inputUser = Convert.ToInt32(Console.ReadLine());
-
-                                if (inputUser < 40 || inputUser > 90)
-                                {
-                                    Console.WriteLine("Error,follow the instructions ");
-                                }
-                                else
-                                {
-                                    hp = inputUser;
-                                    break;
-                                }
-
-                            }
-                            while (!validAttackDices)
-                            {
-                                Console.WriteLine("\nChoose the number of attack dice between 6-9 d6");
-                                int inputUser = Convert.ToInt32(Console.ReadLine());
-
-                                if (inputUser < 6 || inputUser > 9)
-                                {
-                                    Console.WriteLine("Error,follow the instructions ");
-                                }
-                                else
-                                {
-                                    attackDices = inputUser;
-                                    break;
-                                }
-                            }
-
-                            Menu.FarmersList().Add(new Farmer(username, hp, attackDices, types));
-                            Console.Clear();
-                            Console.WriteLine($"\n{types} {username} create with {hp} HP and {attackDices} attack\n");
+                            AddChemFarmer.ChemFarmer();
                             break;
 
                         case ConsoleKey.D3:
                         case ConsoleKey.NumPad3:
-
-
-                            inTractor = true;
-                            types = "Tractor Farmer";
-                            Console.Clear();
-                            Console.Write("\nName: ");
-                            username = Console.ReadLine();
-
-                            while (!validHp)
-                            {
-                                Console.WriteLine("\nChoose between 100 and 150 life points ");
-                                int inputUser = Convert.ToInt32(Console.ReadLine());
-
-                                if (inputUser < 100 || inputUser > 150)
-                                {
-                                    Console.WriteLine("Error,follow the instructions ");
-                                }
-                                else
-                                {
-                                    hp = inputUser;
-                                    break;
-                                }
-
-                            }
-                            while (!validAttackDices)
-                            {
-                                Console.WriteLine("\nChoose the number of attack dice between 1-3 d6");
-                                int inputUser = Convert.ToInt32(Console.ReadLine());
-
-                                if (inputUser < 1 || inputUser > 3)
-                                {
-                                    Console.WriteLine("Error,follow the instructions ");
-                                }
-                                else
-                                {
-                                    attackDices = inputUser;
-                                    break;
-                                }
-                            }
-
-                            Menu.FarmersList().Add(new Farmer(username, hp, attackDices, types));
-                            Console.Clear();
-                            Console.WriteLine($"\n{types} {username} create with {hp} HP and {attackDices} attack\n");
+                            AddTractorFarmer.TractorFarmer();
                             break;
 
                         case ConsoleKey.D0:
