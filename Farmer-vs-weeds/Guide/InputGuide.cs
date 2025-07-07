1b248dbf3e96ed36acb2c87a526625c457cf2e02
HazelCunuder <basile.cunuder@gmail.com>
2025-07-07 15:24:30 +0200

    feat: add menu guide and classe guide

---
+++ Farmer-vs-weeds/Guide/InputGuide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmer_vs_weeds.Guide
+{
+    internal class InputGuide
+    {
+        public static void InputGuideDisplay()
+        {
+
+        }
+    }
+}
---
+++ Farmer-vs-weeds/Guide/MenuGuide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmer_vs_weeds.Guide
+{
+    internal class MenuGuide
+    {
+        public static void GuideMenu()
+        {
+
+        }
+    }
+}
