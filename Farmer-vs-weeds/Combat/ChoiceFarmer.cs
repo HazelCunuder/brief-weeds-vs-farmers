25366a86da5b52de2e6eb56b6b011c64b586c731
Loicwr <nephtyse19@hotmail.fr>
2025-07-07 10:36:08 +0200

    feature (Combat) : add class choiceFarmer

---
+++ Farmer-vs-weeds/Combat/ChoiceFarmer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmer_vs_weeds.Combat
+{
+    internal class ChoiceFarmer
+    {
+        public static void SelectFarmer(List<Farmer> farmers, string playerName)
+        {
+            int index = -1;
+
+            while (true)
+            {
+                Console.WriteLine($"\n{playerName}, choose your farmer :");
+
+
+            }
+        }
+    }
+}
