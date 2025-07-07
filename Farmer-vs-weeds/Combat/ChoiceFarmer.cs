4cd0b58c04a7847b37cc7eb4fb159672af45e242
Loicwr <nephtyse19@hotmail.fr>
2025-07-07 10:44:07 +0200

    feature (Combat) : Add input user

--- Farmer-vs-weeds/Combat/ChoiceFarmer.cs
+++ Farmer-vs-weeds/Combat/ChoiceFarmer.cs
@@ -11,10 +11,13 @@ namespace Farmer_vs_weeds.Combat
         public static void SelectFarmer(List<Farmer> farmers, string playerName)
         {
             int index = -1;
+            int InputUser;
 
             while (true)
             {
                 Console.WriteLine($"\n{playerName}, choose your farmer :");
+                    InputUser = Convert.ToInt32(Console.ReadLine());
+
 
 
             }
